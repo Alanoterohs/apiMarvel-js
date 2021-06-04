eb2d97dddaab2e6051af4188db8a91db888be283
Alanoterohs <alanoterohs@gmail.com>
2021-06-04 17:39:15 -0300

    fix Search
    dejo funcionando el search de los superheroes :)

--- heros.js
+++ heros.js
@@ -1,15 +1,45 @@
-const url = 'https://developer.marvel.com/';
-const publiKey =  '58874dd85578967d1521626abeb93445';
-const hash = '96fad7526953ba1f2edc9b0c284c7734';
-let contentDiv = '';
-let mainDiv = document.querySelector('.heros');
+//const url = 'https://developer.marvel.com/';
+const URL = `&limit=7&ts=1&apikey=58874dd85578967d1521626abeb93445&hash=96fad7526953ba1f2edc9b0c284c7734`;
+let divHeros = document.querySelector('.heros');
 let body = document.querySelector('.app');
 let ul = document.querySelector('.list-heros');
 let paginationDiv = document.querySelector('.pagination');
 let aHero = document.querySelector('a-hero');
 
+function displayHeros(data) {
+  const displayHero = `
+  <div class= "div-aHero">
+  <div class="div-aHero">
+  <h3 class="title">${data.name}</h3>
+  <img
+  class = "img-aHero"
+  alt="${data.name}"
+  src="${data.thumbnail.path}.${data.thumbnail.extension}"
+  id="${data.id}">
+  </div>
+  <div class= "description-hero"><span>${data.description}</span>
+  </div>
+  <a class= "home-hero" href="index.html">Home</a>
+  </div>
+  `;
+  return displayHero;
+}
 
-function showHeros(data, offset) {
+function getAllHeros(set) {
+  const herosURL = `https://gateway.marvel.com:443/v1/public/characters?offset=${set}&${URL}`;
+
+  fetch(herosURL)
+  .then(res => res.json())
+  .then(json => {
+    pagination(json.data);
+    const heros = json.data.results;
+    heros.map(data => {
+      showHeros(data);
+    });
+  });
+}
+
+function showHeros(data) {
   let newDiv = document.createElement('div');
   newDiv.classList.add('cards-heros');
   newDiv.innerHTML = `
@@ -23,68 +53,63 @@ function showHeros(data, offset) {
   id="${data.id}">
   </li>
   `;
-  mainDiv.appendChild(newDiv);
+  divHeros.appendChild(newDiv);
 }
-
-
-function getAllHeros(set) {
-  let limit = 7;
-  const herosURL =
-  `https://gateway.marvel.com:443/v1/public/characters?offset=${set}&limit=${limit}&ts=1&apikey=${publiKey}&hash=${hash}`;
-  fetch(herosURL)
-  .then(res => res.json())
-  .then(json => {
-    console.log(json);
-    pagination(json.data);
-    const heros = json.data.results;
-    let offset = json.data;
-    heros.map(data => {
-      showHeros(data, offset);
-    });
-  });
-}
-
-
 
 function showAHero(id) {
-  const aHeroURL = `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=1&apikey=${publiKey}&hash=${hash}`
+  const aHeroURL = `https://gateway.marvel.com:443/v1/public/characters/${id}?${URL}`;
+
   fetch(aHeroURL)
   .then(res => res.json())
   .then(json => {
     const aHero = json.data.results[0];
-    const newDivToHero = document.createElement('div');
+    let newDivToHero = document.createElement('div');
     newDivToHero.classList.add('aHero');
-    newDivToHero.innerHTML = `
-    <div class= "div-aHero">
-    <div class="div-aHero">
-    <h3 class="title">${aHero.name}</h3>
-    <img
-    class = "img-aHero"
-    alt="${aHero.name}"
-    src="${aHero.thumbnail.path}.${aHero.thumbnail.extension}"
-    id="${aHero.id}">
-    </div>
-    <div class= "description-hero"><span>${aHero.description}</span>
-    </div>
-    <a class= "home-hero" href="index.html">Home</a>
-    </div>
-    `;
+
+    const dataHero = displayHeros(aHero);
+    newDivToHero.innerHTML = dataHero;
     body.appendChild(newDivToHero);
-    mainDiv.style.display = 'none';
+
+    divHeros.style.display = 'none';
     paginationDiv.style.display = 'none';
   });
 };
 
-
 getAllHeros();
 
 const filterSearch = document.querySelector('.search');
-
 filterSearch.addEventListener('keypress', (event) => {
   if (event.keyCode == 13) {
-    filterHero(filterSearch.value);
+    console.log(filterSearch.value);
+    search(filterSearch.value);
   }
 });
+
+function search(name) {
+  const searchHero =
+  `https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=${name}&${URL}`;
+  fetch(searchHero)
+  .then(res => res.json())
+  .then(json => {
+    const searchHero = json.data.results[0];
+    let divSearchHero = document.createElement('div');
+
+    const dataSearchHero = displayHeros(searchHero);
+    divSearchHero.innerHTML = dataSearchHero;
+    body.appendChild(divSearchHero);
+
+    divHeros.style.display = 'none';
+    paginationDiv.style.display = 'none';
+  });
+}
+
+
+
+
+
+
+
+
 
 function back() {
   let set = parseInt(sessionStorage.getItem('id'));
@@ -101,7 +126,6 @@ function next() {
 };
 
 function pagination(data) {
-  console.log(data);
   var page = data.offset;
   var limit = data.limit;
   var total = Math.ceil(data.total / limit);
@@ -117,9 +141,28 @@ function pagination(data) {
   nextPage.addEventListener('click', next);
   space.textContent = ` page number ${page} of the ${total} `;
 
-
+  //divHeros.style.display = 'none';
+  //paginationDiv.style.display = 'none';
   paginationDiv.appendChild(newDivPag);
   newDivPag.appendChild(backPage);
   newDivPag.appendChild(nextPage);
   paginationDiv.appendChild(space);
 };
+
+
+
+// newDivToHero.innerHTML = `
+// <div class= "div-aHero">
+// <div class="div-aHero">
+// <h3 class="title">${aHero.name}</h3>
+// <img
+// class = "img-aHero"
+// alt="${aHero.name}"
+// src="${aHero.thumbnail.path}.${aHero.thumbnail.extension}"
+// id="${aHero.id}">
+// </div>
+// <div class= "description-hero"><span>${aHero.description}</span>
+// </div>
+// <a class= "home-hero" href="index.html">Home</a>
+// </div>
+// `;
