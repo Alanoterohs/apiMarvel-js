2e2ada6dc6aa462ca3e9afc2e3bfa02c2f8cd1fd
Alanoterohs <alanoterohs@gmail.com>
2021-06-08 17:54:00 -0300

    functionality ready
    Funcionalidad realizada

--- heros.js
+++ heros.js
@@ -1,10 +1,16 @@
 //const url = 'https://developer.marvel.com/';
-const URL = `&limit=7&ts=1&apikey=58874dd85578967d1521626abeb93445&hash=96fad7526953ba1f2edc9b0c284c7734`;
-let divHeros = document.querySelector('.heros');
-let body = document.querySelector('.app');
-let ul = document.querySelector('.list-heros');
-let paginationDiv = document.querySelector('.pagination');
-let aHero = document.querySelector('a-hero');
+//const URL = `&limit=7&ts=1&apikey=58874dd85578967d1521626abeb93445&hash=96fad7526953ba1f2edc9b0c284c7734`;
+const URL = `&ts=1&apikey=58874dd85578967d1521626abeb93445&hash=96fad7526953ba1f2edc9b0c284c7734`;
+// const mainHeros = document.querySelector('#heros');
+// const containerAllHeros = document.querySelector('#allHeros');
+// const sectionHero = document.querySelector('#section-hero');
+//const divHeros = document.querySelector('.heros');
+//const allHeros = document.querySelector('.allHeros');
+const body = document.querySelector('.app');
+//const sectionHero = document.querySelector('.section-hero');
+// const paginationDiv = document.querySelector('#pagination');
+const aHero = document.querySelector('a-hero');
+
 
 function displayHeros(data) {
   const displayHero = `
@@ -25,38 +31,6 @@ function displayHeros(data) {
   return displayHero;
 }
 
-function getAllHeros(set) {
-  const herosURL = `https://gateway.marvel.com:443/v1/public/characters?offset=${set}&${URL}`;
-
-  fetch(herosURL)
-  .then(res => res.json())
-  .then(json => {
-    pagination(json.data);
-    const heros = json.data.results;
-    heros.map(data => {
-      showHeros(data);
-    });
-  });
-}
-
-function showHeros(data) {
-  // divHeros.style.display = 'none';
-  // paginationDiv.style.display = 'none';
-  let newDiv = document.createElement('div');
-  newDiv.classList.add('cards-heros');
-  newDiv.innerHTML = `
-  <li class="listCard-class">
-  <h3 class="title">${data.name}</h3>
-  <img
-  class = "img-class"
-  onclick = "showAHero(${data.id})"
-  src="${data.thumbnail.path}.${data.thumbnail.extension}"
-  alt="${data.name}"
-  id="${data.id}">
-  </li>
-  `;
-  divHeros.appendChild(newDiv);
-}
 
 function showAHero(id) {
   const aHeroURL = `https://gateway.marvel.com:443/v1/public/characters/${id}?${URL}`;
@@ -65,52 +39,16 @@ function showAHero(id) {
   .then(res => res.json())
   .then(json => {
     const aHero = json.data.results[0];
-    let newDivToHero = document.createElement('div');
+    const newDivToHero = document.createElement('div');
     newDivToHero.classList.add('aHero');
 
     const dataHero = displayHeros(aHero);
     newDivToHero.innerHTML = dataHero;
     body.appendChild(newDivToHero);
 
-    divHeros.style.display = 'none';
+    mainHeros.style.display = 'none';
     paginationDiv.style.display = 'none';
   });
-};
-
-function pagination(data) {
-  var page = data.offset;
-  var limit = data.limit;
-  var total = Math.ceil(data.total / limit);
-
-  let newDivPag = document.createElement('div');
-  let backPage = document.createElement('button');
-  let nextPage = document.createElement('button');
-  let space = document.createElement('span');
-
-  backPage.textContent = '<- Back';
-  backPage.addEventListener('click', back);
-  nextPage.textContent = 'Next ->';
-  nextPage.addEventListener('click', next);
-  space.textContent = ` page number ${page} of the ${total} `;
-
-  paginationDiv.appendChild(newDivPag);
-  newDivPag.appendChild(backPage);
-  newDivPag.appendChild(nextPage);
-  paginationDiv.appendChild(space);
-};
-
-function back() {
-  let offset = parseInt(sessionStorage.getItem('offset'));
-  offset -= 8;
-  sessionStorage.setItem('offset', offset);
-  getAllHeros(offset);
-};
-
-function next() {
-  let offset = parseInt(sessionStorage.getItem('offset'));
-  offset += 8;
-  sessionStorage.setItem('offset', offset);
-  getAllHeros(offset);
 };
 
 const filterSearch = document.querySelector('.search');
@@ -134,9 +72,99 @@ function search(name) {
     divSearchHero.innerHTML = dataSearchHero;
     body.appendChild(divSearchHero);
 
-    divHeros.style.display = 'none';
+    mainHeros.style.display = 'none';
     paginationDiv.style.display = 'none';
   });
 }
 
-getAllHeros(0); //seteo el offset
+const mainHeros = document.querySelector('#heros');
+const containerAllHeros = document.querySelector('#allHeros');
+const sectionHero = document.querySelector('#section-hero');
+
+function hide(section) {
+  section.className = 'hidden';
+}
+
+function show(section, classname) {
+  section.className = classname;
+}
+
+function getAllHeros(offset, container) {
+  fetch(`https://gateway.marvel.com:443/v1/public/characters?orderBy=name&offset=${offset}&${URL}`)
+  .then(res => res.json())
+  .then(json => {
+      pagination(json);
+      console.log(json);
+      json.data.results.map((data) =>
+        showHeros(data, container)
+      );
+      return json.data.results;
+    });
+}
+
+function back() {
+  let offset = parseInt(sessionStorage.getItem('offset'));
+  offset -= 8;
+  sessionStorage.setItem('offset', offset);
+  getAllHerosFunction(offset);
+};
+
+function next() {
+  let offset = parseInt(sessionStorage.getItem('offset'));
+  offset += 8;
+  sessionStorage.setItem('offset', offset);
+  getAllHerosFunction(offset);
+};
+
+const paginationDiv = document.querySelector('#pagination');
+
+function showHeros(data, container) {
+  const newDiv = document.createElement('div');
+  newDiv.classList.add('cards-heros');
+  newDiv.innerHTML = `
+  <li class="listCard-class">
+  <h3 class="title">${data.name}</h3>
+  <img
+  class = "img-class"
+  onclick = "showAHero(${data.id})"
+  src="${data.thumbnail.path}.${data.thumbnail.extension}"
+  alt="${data.name}"
+  id="${data.id}">
+  </li>
+  `;
+  container.appendChild(newDiv);
+}
+
+function pagination(data) {
+  let page = data.offset;
+  let limit = data.limit;
+  let total = Math.ceil(data.total / limit);
+
+  paginationDiv.innerHTML= '';
+
+  let newDivPag = document.createElement('div');
+  let backPage = document.createElement('button');
+  let nextPage = document.createElement('button');
+  let space = document.createElement('span');
+
+  backPage.textContent = '<- Back';
+  backPage.addEventListener('click', back);
+  nextPage.textContent = 'Next ->';
+  nextPage.addEventListener('click', next);
+  space.textContent = ` page number ${page} of the ${total} `;
+
+  paginationDiv.appendChild(newDivPag);
+  newDivPag.appendChild(backPage);
+  newDivPag.appendChild(nextPage);
+  paginationDiv.appendChild(space);
+};
+
+function getAllHerosFunction(offset) {
+	hide(mainHeros);
+	hide(sectionHero);
+	containerAllHeros.innerHTML = '';
+  getAllHeros(offset, containerAllHeros);
+	show(mainHeros, 'section-hero');
+}
+
+getAllHerosFunction(0);
